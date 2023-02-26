e69b21f540e79f3736f089e27c137dae11d7d3ea
Rinnegatamante <rinnegatamante@gmail.com>
2023-02-26 17:56:23 +0100

    Source upload.

---
+++ loader/config.h
@@ -0,0 +1,18 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define DEBUG
+
+#define LOAD_ADDRESS 0x98000000
+
+#define MEMORY_NEWLIB_MB 240
+#define MEMORY_VITAGL_THRESHOLD_MB 12
+
+#define DATA_PATH "ux0:data/funky"
+#define SO_PATH DATA_PATH "/" "libAndroidGame.so"
+#define CONFIG_FILE_PATH "app0:settings.cfg"
+
+#define SCREEN_W 960
+#define SCREEN_H 544
+
+#endif
